7619c1f131c78af5df0f8174a8edeeb1568b7e60
kulsxyz7 <kulsxyz7@gmail.com>
2020-03-20 03:38:21 +0700

    menambahkan file books_code.sql

---
+++ books/book_code.sql
@@ -0,0 +1,11 @@
+-- SELECT 
+--     CONCAT (
+--         SUBSTRING(title, 1, 10),
+--         '...'
+--     ) AS 'short title'
+-- FROM books;
+
+
+SELECT
+    SUBSTRING(REPLACE(title, 'e', '3'), 1, 10) AS 'weird string'
+FROM books;
