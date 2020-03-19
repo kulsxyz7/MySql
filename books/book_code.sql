598815989a3d78ee8ae9c284d0e17296ac6d61bf
kulsxyz7 <kulsxyz7@gmail.com>
2020-03-20 05:03:19 +0700

    menambahkan file testing/exercises.sql

--- books/book_code.sql
+++ books/book_code.sql
@@ -9,3 +9,16 @@
 SELECT
     SUBSTRING(REPLACE(title, 'e', '3'), 1, 10) AS 'weird string'
 FROM books;
+
+'woof' 'foow'
+SELECT CONCAT ('woof', REVERSE ('foow'));
+
+"Eggers is 6 character long"
+
+SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname),' character long')
+FROM books;
+
+SELECT UPPER(title)
+FROM books;
+
+SELECT CONCAT('MY FAVORITE BOOK IS THE ', UPPER(title)) FROM books;
