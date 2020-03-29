03e30c7564fff07a5833d27bc7aa2fd83661eb79
kulsxyz7 <kulsxyz7@gmail.com>
2020-03-29 23:46:15 +0700

    menambah file one_to_many

---
+++ one_to_many/table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE customers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+CREATE TABLE orders(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT
+);
